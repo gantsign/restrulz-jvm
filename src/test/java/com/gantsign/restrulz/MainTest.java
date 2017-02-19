b46703e165d8e1caf933c666f52dfb29ecbfac86
John Freeman <john.freeman.opensource@gmail.com>
2017-02-19 15:36:00 +0000

    Initial commit after Maven archetype
    Initial commit after running: `com.github.gantsign.java-application-maven-archetype:java-application-maven-archetype` Maven archetype.

---
+++ src/test/java/com/gantsign/restrulz/MainTest.java
@@ -0,0 +1,34 @@
+package com.gantsign.restrulz;
+
+import static com.google.common.truth.Truth.assertThat;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Test;
+
+public class MainTest {
+
+  @Test
+  public void main()
+      throws Exception {
+
+    PrintStream out = System.out;
+    try {
+      ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+      System.setOut(new PrintStream(buffer, true, "UTF-8"));
+
+      Main.main(new String[0]);
+
+      String actual = new String(buffer.toByteArray(), UTF_8).trim();
+      assertThat(actual).endsWith(".Main - Hello, World!");
+    } finally {
+      System.setOut(out);
+    }
+  }
+
+  @Test
+  public void instantiate() {
+    new Main();
+  }
+}
